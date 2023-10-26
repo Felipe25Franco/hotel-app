ad2a7fd5a9eee118f2507bdf3badd44cce6f32f2
Felipe Franco <felipejf739@hotmail.com>
2023-10-26 12:51:03 -0300

    save

--- src/views/listagens/listagem-cliente.js
+++ src/views/listagens/listagem-cliente.js
@@ -12,9 +12,9 @@ import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 
-import axios from '../../config/axios';
+import axios from 'axios';
 
-import { BASE_URL } from 'axios';
+import { BASE_URL } from '../../config/axios';
 
 const baseURL = `${BASE_URL}/cliente`;
 
