f59effed73ccba5e1c8afcf01ad894f539db7ebf
Felipe Franco <felipejf739@hotmail.com>
2023-10-31 14:55:15 -0300

    save

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -52,6 +52,19 @@ function Navbar(props) {
           <ul className='navbar-nav'>
             <NavbarItem render='true' href='/listagem-hospedagem' label='Hospedagens' />
           </ul>
+          
+            <ul class="navbar-nav">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Separated link</a>
+              </div>
+            </ul>
+          
+          
         </div>
       </div>
     </div>
